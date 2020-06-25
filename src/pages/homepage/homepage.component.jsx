f4d889376026b0e3657d66b05162eb452d5154e0
Vipin GS <vipings@aol.in>
2020-06-25 12:49:44 +0530

    added style changes

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './homepage.styles.css';
-import { Switch , Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import DefectDetails from '../../components/defectdetails/defectdetails.component';
 import Header from '../../components/header/header.component';
 
-const HomePage = ({match}) => {
-    return (  
-            <div>
-                <Header />
+const HomePage = ({ match }) => {
+    return (
+        <div className="homepage-main">
+            <Header />
             <Switch>
-                <Route path={`${match.path}`} component={DefectDetails}/>
+                <Route path={`${match.path}`} component={DefectDetails} />
             </Switch>
         </div>
     )
